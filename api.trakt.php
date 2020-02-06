e2672506ba93cc74440d2c0877c45bd41aa98362
Laim <laimmckenzie@icloud.com>
2020-02-06 13:28:01 +0000

    v1.0.0.0
    I hate this.  Don't use this, it's horrendous.

--- api.trakt.php
+++ api.trakt.php
@@ -1,66 +1,147 @@
 <?php
-// Author: Laim McKenzie
-// Repo: https://github.com/laim/TraktPHPScrobble
-// License: MIT
+//       Author: Laim McKenzie
+//        Repo: https://github.com/laim/TraktPHPScrobble
+//     License: MIT
+// Description: Returns either the current watching status (show/movie) or most recently watched
 
-$k = "TRAKT_API_KEY";
-$u = "TRAKT_USER";
+// API Configuration //
+$key = "api_key";
+$user = "laim";
+// API Configuration //
 
-function trakt($u, $t, $k) {
+$trakt = array();
 
-    /**
-  * 
+function traktAPI($user, $key, $type) {
+  /**
   * Returns t/f if user is scrobbling
-  *
-  * @param string $k  trakt key
-  * @param string $u  trakt username
-  * @param string $t  trakt type (watching, history etc)
+  * @param string $user   trakt username
+  * @param string $key    trakt key
+  * @param string $type   trakt type (watching, history etc)
   * @return array json_decode response
   */
-
   $ch = curl_init();
-
-  curl_setopt($ch, CURLOPT_URL, "https://api.trakt.tv/users/$u/$t");
+  curl_setopt($ch, CURLOPT_URL, "https://api.trakt.tv/users/$user/$type");
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     "Content-Type: application/json",
     "trakt-api-version: 2",
-    "trakt-api-key: $k"
+    "trakt-api-key: $key"
   ));
-
   $response = curl_exec($ch);
   curl_close($ch);
-
   return json_decode($response, true);
 }
 
-function scrobbleStatus($k, $u) {
+function traktScrobbleStatus($user, $key) {
   /**
-  * 
   * Returns t/f if user is scrobbling
-  *
-  * @param string $k  trakt key
-  * @param string $u  trakt username
+  * @param string $user   trakt username
+  * @param string $key    trakt key
   * @return boolean
   */
+  return ! empty(traktAPI($user, $key, "watching")['expires_at']);
+}
 
-  if(empty(trakt($u, "watching", $k)['expires_at'])) {
-    return false;
-  } else {
-    return true;
+function traktScrobbleType($user, $key) {
+  /**
+  * Returns string if user is scrobbling
+  * @param string $user   trakt username
+  * @param string $key    trakt key
+  * @return string
+  */
+
+  $trakt_response = traktAPI($user, $key, "watching")['type'];
+  if($trakt_response == "movie") {
+    return "watching_movie";
+  } elseif($trakt_response == "episode") {
+    return "watching_tvshow";
   }
 }
 
-function traktResponse($k, $u) {
-  if(scrobbleStatus($k, $u)) {
-    return trakt($u, "watching", $k);
+function traktScrobbleResponse($user, $key) {
+  /**
+  * Returns string if user is scrobbling
+  * @param string $user   trakt username
+  * @param string $key    trakt key
+  * @return string
+  */
+  if(traktScrobbleStatus($user, $key)) { // if the user is scrobbling
+    if(traktScrobbleType($user, $key) == "watching_movie") { // if the user is watching a movie
+      
+      $trakt_response = traktAPI($user, $key, "watching");
+      $trakt = [
+        "started_at" => $trakt_response['started_at'],
+        "finishes_at" => $trakt_response['expires_at'],
+        "type" => $trakt_response['type'],
+        "movie" => array (
+          "title" => $trakt_response['movie']['title'],
+          "year" => $trakt_response['movie']['year'],
+          "trakt-id" => $trakt_response['movie']['ids']['trakt']
+        )
+      ];
+      return $trakt;
+
+    } elseif(traktScrobbleType($user, $key) == "watching_tvshow") {
+      
+      $trakt_response = traktAPI($user, $key, "watching");
+      $trakt = [
+        "started_at" => $trakt_response['started_at'],
+        "finishes_at" => $trakt_response['expires_at'],
+        "type" => $trakt_response['type'],
+        "episode" => array (
+          "season" => $trakt_response['episode']['season'],
+          "episode_number" => $trakt_response['episode']['number'],
+          "title" => $trakt_response['episode']['title'],
+          "trakt-id" => $trakt_response['episode']['ids']['trakt']
+        )
+      ];
+      return $trakt;
+
+    }
   } else {
-    return trakt($u, "history", $k);
-  };
+
+    $trakt_response = traktAPI($user, $key, "history");
+    $x = 0;
+    while($x < 5) {
+      if($trakt_response[$x]['type'] == "movie") {
+        $trakt[] = [
+          "watched_at" => $trakt_response[$x]['watched_at'],
+          "type" => $trakt_response[$x]['type'],
+          "movie" => array (
+            "title" => $trakt_response[$x]['movie']['title'],
+            "year" => $trakt_response[$x]['movie']['year'],
+            "trakt-id" => $trakt_response[$x]['movie']['ids']['trakt']
+          )
+          ];
+      } else {
+        $trakt[] = [
+          "watched_at" => $trakt_response[$x]['watched_at'],
+          "type" => $trakt_response[$x]['type'],
+          "show" => array (
+            "show_name" => $trakt_response[$x]['show']['title'],
+            "show_year" => $trakt_response[$x]['show']['year'],
+            "trakt-id" => $trakt_response[$x]['show']['ids']['trakt']
+          ),
+          "episode" => array (
+            "episode_season" => $trakt_response[$x]['episode']['season'],
+            "episode_number" => $trakt_response[$x]['episode']['number'],
+            "episode_title" => $trakt_response[$x]['episode']['title'],
+            "trakt-id" => $trakt_response[$x]['episode']['ids']['trakt']
+          )
+        ];
+      }
+      $x++;
+    }
+  
+    return $trakt;
+    
+  }
 }
 
-var_dump(traktResponse($k, $u));
+/*
+uncomment this section if you want to just test output
+echo '<pre>' , print_r(traktScrobbleResponse($user, $key), true) , '</pre>';
+*/
 
 ?>
